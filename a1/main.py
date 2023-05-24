7295feab522a9b793888729c55f4e1c7e0842e9c
Lukas Briem <lukas.briem.lb@gmail.com>
2023-05-24 16:18:07 +0200

    median for sorted list added in calculator language

--- a1/main.py
+++ a1/main.py
@@ -24,9 +24,18 @@ def main() -> None:
     calculator = Calculator()
     calculator.register["a"] = '(Welcome)"b@'
     calculator.register["b"] = '\'@b@'
-    calculator.register["c"] = '\'@##1-0()(7!7$5!5$+6!6$6!6$1-2!_~5+$4!4$()6!6$2!@)2!@1$1$2$3!3$/"'
+    calculator.register["c"] = '\'@##1-0()(7!7$5!5$+6!6$6!6$1-2!_~5+$4!4$()6!6$2!@)2!@1$1$2$3!3$/"'  # mean
     calculator.register["d"] = '#1+()(4!4$1-2!_~3+$()4!4$2!@\\")2!@"'  # delete elemnts on stack and print them
-    calculator.register["e"] = '\'2!@@#1+!@#$#2/#2/0()(7!7$5!5$+6!6$6!6$1-2!_~5+$4!4$()6!6$2!@)2!@1$1$2$3!3$/#2-#2-0()(8!8$8!-2!*5!5$+7!7$7!7$7!7$1-2!_~6+$5!5$()7!7$2!@)2!@1$1$2$3!3$/2$"'
+    calculator.register[
+        "e"] = '\'2!@@#1+!@#$#2/#2/0()(7!7$5!5$+6!6$6!6$1-2!_~5+$4!4$()6!6$2!@)2!@1$1$2$3!3$/#2-#2-0()(' \
+               '8!8$8!-2!*5!5$+7!7$7!7$7!7$1-2!_~6+$5!5$()7!7$2!@)2!@1$1$2$3!3$/2$"'  # variance
+
+    calculator.register["f"] = "\'@#(#2/#2/1+#2+-~!3!3$#-~!+2/)(#2/#-~!)4!4$2%3-~!2$2$@@#1-()(4!4$5$1-2!_~3+$()4!4$2!@)2!@1$1$1$\""  # median for sorted lists
+
+    # MEDIAN attempts on unsorted list
+    # calculator.register["g"] = "(7!7$0 5!5$10!10$2!2!2/>)"
+    # calculator.register["h"] = "(##1-#2-0 0()(8!8$0 5!5$11!11$7!4!4!2/1+=&7!7$4!4!2/>&|)()(Code)8!10+!10!12+!2!2!=2!4!5!<|3$9!9$+10!10$1-2!_~8+$8!8$@)"
+
     for token in calculator.register["a"][::-1]:
         calculator.cmd.push(token)
     calculator.run()
