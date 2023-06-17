7c2b3f1b63d59931fa2fc8b5337a99d09dc20a60
Paul Florian Sattlegger <paulsattlegger1@gmail.com>
2023-06-17 02:02:03 +0200

    Add header and refactor keyEvent

--- a3/app/Main.hs
+++ a3/app/Main.hs
@@ -5,10 +5,10 @@ module Main (main) where
 
 import Brick.AttrMap qualified as A
 import Brick.Main qualified as M
-import Brick.Types (Widget)
 import Brick.Types qualified as T
 import Brick.Util (on)
-import Brick.Widgets.Core (strWrap)
+import Brick.Widgets.Center (hCenter)
+import Brick.Widgets.Core (str, strWrap, withAttr, (<=>))
 import Control.Monad (void)
 import Graphics.Vty qualified as V
 import Lens.Micro.Platform
@@ -18,41 +18,53 @@ data EditorState n = EditorState {_text :: String, _index :: Int}
 
 makeLenses ''EditorState
 
-drawUI :: EditorState () -> [Widget ()]
-drawUI s = [strWrap (before ++ "_" ++ after)]
+drawUI :: EditorState () -> [T.Widget ()]
+drawUI s = [ui]
   where
     (before, after) = splitAt (s ^. index) (s ^. text)
+    ui =
+      withAttr (A.attrName "highlight") (hCenter $ str "Syntax-Aware Editor")
+        <=> strWrap (before ++ "_" ++ after)
 
 appEvent :: T.BrickEvent () e -> T.EventM () (EditorState ()) ()
 appEvent (T.VtyEvent (V.EvKey e [])) = keyEvent e
 appEvent _ = return ()
 
 keyEvent :: V.Key -> T.EventM () (EditorState ()) ()
-keyEvent (V.KChar c) = do
+keyEvent (V.KChar c) = insertChar c
+keyEvent V.KEnter = insertChar '\n'
+keyEvent V.KBS = deleteChar
+keyEvent V.KRight = incrementIndex
+keyEvent V.KLeft = decrementIndex
+keyEvent V.KEsc = M.halt
+keyEvent _ = return ()
+
+deleteChar :: T.EventM () (EditorState ()) ()
+deleteChar = do
+  cursor <- use index
+  text %= \i -> delete' cursor i
+  decrementIndex
+
+insertChar :: Char -> T.EventM () (EditorState ()) ()
+insertChar c = do
   cursor <- use index
   text %= \i -> insert' cursor c i
   index += 1
-keyEvent V.KEnter = do
-  cursor <- use index
-  text %= \i -> insert' cursor '\n' i
-  index += 1
-keyEvent V.KBS = do
-  cursor <- use index
-  text %= \i -> delete' cursor i
+
+decrementIndex :: T.EventM () (EditorState ()) ()
+decrementIndex = do
   index %= \i -> max 0 (i - 1)
-keyEvent V.KEsc = M.halt
-keyEvent V.KRight = do
+
+incrementIndex :: T.EventM () (EditorState ()) ()
+incrementIndex = do
   textLength <- use (text . to length)
   index %= \i -> min (i + 1) textLength
-keyEvent V.KLeft = do
-  index %= \i -> max 0 (i - 1)
-keyEvent _ = return ()
 
 initialState :: EditorState ()
 initialState = EditorState "" 0
 
 theMap :: A.AttrMap
-theMap = A.attrMap V.defAttr []
+theMap = A.attrMap V.defAttr [(A.attrName "highlight", V.black `on` V.white)]
 
 editor :: M.App (EditorState ()) e ()
 editor =
