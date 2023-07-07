49c06632813db7d36608ba37844752cc5844d22a
Paul Florian Sattlegger <paulsattlegger1@gmail.com>
2023-07-07 04:32:22 +0200

    Refactor and fix remaining TODOs

--- a3/app/Main.hs
+++ a3/app/Main.hs
@@ -13,75 +13,104 @@ import Brick.Widgets.Core (Padding (Max), emptyWidget, padTop, str, strWrap, vBo
 import Control.Applicative ((<|>))
 import Control.Exception
 import Control.Monad.IO.Class
-import Data.Char
 import Data.Functor
 import Data.Maybe (fromMaybe)
-import Data.List (nub)
 import Graphics.Vty qualified as V
 import Lens.Micro.Platform
 import Lib
 import Parser
 import System.Environment (getArgs)
 
--- TODO: Document everything
--- TODO: Fix indentation (in `splitNewlines` replace "[("default", " ", i)]" with [] but then no empty lines are possible)
-data EditorState n = EditorState {_path :: FilePath, _index :: Int, _changed :: Bool, _text :: String}
+data EditorState n = EditorState {_text :: String, _index :: Int, _path :: FilePath, _changed :: Bool}
 
 makeLenses ''EditorState
 
--- TODO: Display cursor above parsed output
+-- | 'drawUI' is the main rendering function that creates the overall user interface.
+-- It consists of two parts:
+-- 1. A header that displays the current file path and an indicator if there are unsaved changes.
+-- 2. The main text area of the editor which displays the contents of the file.
+-- 3. A parser area, that validates the text and displays the result of the validation.
 drawUI :: EditorState () -> [T.Widget ()]
-drawUI s = [ui, parser]
+drawUI s =
+  [ drawHeader s <=> drawText s,
+    drawParser s
+  ]
+
+-- | 'drawHeader' creates a header widget for the syntax-aware editor.
+-- The header displays the current file path and an asterisk if there are unsaved changes.
+drawHeader :: EditorState () -> T.Widget n
+drawHeader s = withAttr (A.attrName "highlight") (hCenter $ str $ "Syntax-Aware Editor [" ++ s ^. path ++ (if s ^. changed then "*" else "") ++ "]")
+
+-- | 'drawText' creates the main text widget for the syntax-aware editor.
+-- It splits the text by new lines and applies highlighting to syntax elements.
+drawText :: EditorState () -> T.Widget n
+drawText s = drawLines (splitNewlines (highlightBrace (highlightBraceAtCursor (highlightNameAtCursor (currentWord before after) attrList) braceIndex)))
   where
     (before, after) = splitAt (s ^. index) (s ^. text)
-    word = currentWord before after
     attrList = addDefaultAttr (splitAlphanumeric (before ++ "_" ++ after))
-    braceIndex = getIndexOfBrace attrList before after
+    braceIndex = indexOfBrace attrList before after
+
+-- | 'drawParser' creates a parser widget for the syntax-aware editor.
+-- This widget shows the parsing status of the text:
+-- If the text is not valid according to the parser, it will display an error message.
+-- If the text is valid, it will display a 'Valid' message.
+drawParser :: EditorState () -> T.Widget n
+drawParser s = padTop Max message
+  where
     message = case parseString (s ^. text) of
       Left err -> withAttr (A.attrName "error") (strWrap err)
       Right _ -> withAttr (A.attrName "valid") (strWrap "Valid")
-    ui =
-      drawHeader s
-        <=> drawLines (splitNewlines (highlightBrace (highlightBraceAtCursor (highlightNameAtCursor word attrList) braceIndex)))
-    parser = padTop Max message
 
--- Returns the index in the AST of the brace that the cursor currently points at
+-- | Draws each line seperated by "\n" into a new vBox
+drawLines :: [[(String, String, Int)]] -> T.Widget n
+drawLines lines' = vBox (map astToWidget lines')
+
+-- | Returns the index in the AST of the brace that the cursor currently points at.
 -- Input: [(Attr, String-part, Index in AST)] -> before -> after -> AST-index
-getIndexOfBrace :: [(String, String, Int)] -> String -> String -> Int
-getIndexOfBrace ast before after
+indexOfBrace :: [(String, String, Int)] -> String -> String -> Int
+indexOfBrace ast before _
   | null before = -1
-  | last before == '(' = findBraceInAst ast "(" (countBraces before '(')
-  | last before == ')' = findBraceInAst ast ")" (countBraces before ')')
-  | last before == '{' = findBraceInAst ast "{" (countBraces before '{')
-  | last before == '}' = findBraceInAst ast "}" (countBraces before '}')
+  | last before == '(' = braceInAst ast "(" (countBraces before '(')
+  | last before == ')' = braceInAst ast ")" (countBraces before ')')
+  | last before == '{' = braceInAst ast "{" (countBraces before '{')
+  | last before == '}' = braceInAst ast "}" (countBraces before '}')
   | otherwise = -1
 
--- Counts how often the given brace type occours in the String
--- Input: Input-String -> BraceType -> Count
-countBraces :: String -> Char -> Int
-countBraces xs x = foldl (\count char -> if char == x then count + 1 else count) 0 xs
-
--- Returns the AST index of given string based on how often it already occured i.e. get the AST-index of the 3rd "(" in the AST
+-- | Returns the AST index of given string based on how often it already occured i.e., get the AST-index of the 3rd "(" in the AST.
 -- Input: AST -> BraceType -> nth occurance -> AST-index
-findBraceInAst :: [(String, String, Int)] -> String -> Int -> Int
-findBraceInAst ast str n = go ast str n 0
+braceInAst :: [(String, String, Int)] -> String -> Int -> Int
+braceInAst ast s n = go ast s n 0
   where
     go [] _ _ _ = error "String not found enough times in AST"
-    go ((_, x, i) : xs) str n count
-      | x == str && count + 1 == n = i
-      | x == str = go xs str n (count + 1)
-      | otherwise = go xs str n count
+    go ((_, x, i) : xs) s' n' count
+      | x == s' && count + 1 == n' = i
+      | x == s' = go xs s' n' (count + 1)
+      | otherwise = go xs s' n' count
 
--- Returns the AST-index of the matching brace if one exists
+-- | Returns the AST-index of the matching brace if one exists.
 -- Input: AST -> AST-Index first brace -> AST-Index second brace
-getMatchingBrace :: [(String, String, Int)] -> Int -> Int
-getMatchingBrace ast i = fromMaybe (-1) match
+matchingBrace :: [(String, String, Int)] -> Int -> Int
+matchingBrace ast i = fromMaybe (-1) match
   where
     pairs = parenPairs ast ++ parenPairsCurly ast
     match = lookup i pairs <|> lookup i (map swap pairs)
     swap (a, b) = (b, a)
 
--- Given an AST-index pair this function highlights both strings at the corresponding index by setting the attribute in the triple
+-- | This function highlights unbalanced braces by checking for each brace if its index is contained in `parenPairs`.
+highlightBrace :: [(String, String, Int)] -> [(String, String, Int)]
+highlightBrace cs = map formatChar cs
+  where
+    formatChar (s, x, i)
+      | x == "(" && not (isIndexInParenPairs i) = ("error", x, i)
+      | x == ")" && not (isIndexInParenPairs i) = ("error", x, i)
+      | x == "{" && not (isIndexInCurlyParenPairs i) = ("error", x, i)
+      | x == "}" && not (isIndexInCurlyParenPairs i) = ("error", x, i)
+      | otherwise = (s, x, i)
+
+    isIndexInParenPairs i = i `elem` listToSet (parenPairs cs)
+    isIndexInCurlyParenPairs i = i `elem` listToSet (parenPairsCurly cs)
+
+-- | Given an AST-index pair this function highlights both strings at the corresponding index by setting the attribute in the triple.
 -- Input: AST-Index pair -> AST -> modified AST
 highlightBraceIndex :: (Int, Int) -> [(String, String, Int)] -> [(String, String, Int)]
 highlightBraceIndex (a, b) = map format
@@ -91,34 +120,15 @@ highlightBraceIndex (a, b) = map format
       | i == b = ("yellow", x, i)
       | otherwise = (s, x, i)
 
--- Highlights matching braces at the current Cursor position
+-- | Highlights matching braces at the current cursor position.
 -- Input: AST -> AST-Index -> modifed AST
 highlightBraceAtCursor :: [(String, String, Int)] -> Int -> [(String, String, Int)]
 highlightBraceAtCursor ast i
   | i == -1 = ast
-  | getMatchingBrace ast i == -1 = ast
-  | otherwise = highlightBraceIndex (i, getMatchingBrace ast i) ast
+  | matchingBrace ast i == -1 = ast
+  | otherwise = highlightBraceIndex (i, matchingBrace ast i) ast
 
--- | 'drawHeader' creates a header widget for the syntax-aware editor.
--- The header displays the current file path and an asterisk if there are unsaved changes.
-drawHeader :: EditorState () -> T.Widget n
-drawHeader s = withAttr (A.attrName "highlight") (hCenter $ str $ "Syntax-Aware Editor [" ++ s ^. path ++ (if s ^. changed then "*" else "") ++ "]")
-
--- Draws each line seperated by "\n" into a new vBox
-drawLines :: [[(String, String, Int)]] -> T.Widget n
-drawLines lines' = vBox (map astToWidget lines')
-
--- Splits an AST into seperate lines on a "\n"
-splitNewlines :: [(String, String, Int)] -> [[(String, String, Int)]]
-splitNewlines = go []
-  where
-    go acc [] = reverse $ map reverse acc
-    go acc ((s, x, i) : xs)
-      | x == "\n" = go ([("default", " ", i)] : acc) xs
-      | otherwise = case acc of
-          [] -> go [[(s, x, i)]] xs
-          (a : as) -> go (((s, x, i) : a) : as) xs
-
+-- | Highlights a given word in a list of colored words.
 highlightNameAtCursor :: String -> [(String, String, Int)] -> [(String, String, Int)]
 highlightNameAtCursor word = map format
   where
@@ -126,11 +136,22 @@ highlightNameAtCursor word = map format
       | x == word = ("green", x, i)
       | otherwise = (s, x, i)
 
--- Adds the "default" attribute to each entry of the input list as well as an increasing unique index
-addDefaultAttr :: [String] -> [(String, String, Int)]
-addDefaultAttr strings = zipWith (\str idx -> ("default", str, idx)) strings [0 ..]
+-- | Splits an AST into seperate lines on a "\n"
+splitNewlines :: [(String, String, Int)] -> [[(String, String, Int)]]
+splitNewlines = go []
+  where
+    go acc [] = reverse $ map reverse acc
+    go acc ((s, x, i) : xs)
+      | x == "\n" = go ([("default", "\0", i)] : acc) xs
+      | otherwise = case acc of
+          [] -> go [[(s, x, i)]] xs
+          (a : as) -> go (((s, x, i) : a) : as) xs
 
--- Converts the AST into Brick widgets that can be displayed on the terminal
+-- | Adds the "default" attribute to each entry of the input list as well as an increasing unique index
+addDefaultAttr :: [String] -> [(String, String, Int)]
+addDefaultAttr strings = zipWith (\s idx -> ("default", s, idx)) strings [0 ..]
+
+-- | Converts the AST into Brick widgets that can be displayed on the terminal
 astToWidget :: [(String, String, Int)] -> T.Widget n
 astToWidget [] = emptyWidget
 astToWidget ((s, x, _) : cs)
@@ -139,15 +160,7 @@ astToWidget ((s, x, _) : cs)
   | s == "yellow" = withAttr (A.attrName "yellow") (str x) <+> astToWidget cs
   | otherwise = str x <+> astToWidget cs
 
-splitAlphanumeric :: String -> [String]
-splitAlphanumeric [] = []
-splitAlphanumeric (x : xs)
-  | isAlphaNum x =
-      let (alphanum, rest) = span isAlphaNum (x : xs)
-       in alphanum : splitAlphanumeric rest
-  | otherwise = [x] : splitAlphanumeric xs
-
--- Returns a list of tuples that contains the indices of valid braces
+-- | Returns a list of tuples that contains the indices of valid braces
 -- From: https://stackoverflow.com/questions/10243290/determining-matching-parenthesis-in-haskell
 -- Input: AST -> [AST-Index pair of matching braces]
 parenPairs :: [(String, String, Int)] -> [(Int, Int)]
@@ -168,35 +181,11 @@ parenPairsCurly = go []
     go (i : is) ((_, "}", j) : cs) = (i, j) : go is cs
     go acc (_ : cs) = go acc cs
 
--- Converts list of index tuples to a set (without duplicates)
-listToSet :: [(Int, Int)] -> [Int]
-listToSet pairs = nub (concatMap (\(start, end) -> [start, end]) pairs)
-
--- Checks if an index is contained in the list
-indexInList :: Int -> [Int] -> Bool
-indexInList index indices = index `elem` indices
-
--- This function highlights unbalanced braces by checking for each brace if its index is contained in `parenPairs`  
-highlightBrace :: [(String, String, Int)] -> [(String, String, Int)]
-highlightBrace cs = map formatChar cs
-  where
-    formatChar (s, x, i)
-      | x == "(" && not (isIndexInParenPairs i) = ("error", x, i)
-      | x == ")" && not (isIndexInParenPairs i) = ("error", x, i)
-      | x == "{" && not (isIndexInCurlyParenPairs i) = ("error", x, i)
-      | x == "}" && not (isIndexInCurlyParenPairs i) = ("error", x, i)
-      | otherwise = (s, x, i)
-
-    isIndexInParenPairs i = indexInList i (listToSet (parenPairs cs))
-    isIndexInCurlyParenPairs i = indexInList i (listToSet (parenPairsCurly cs))
-
-currentWord :: String -> String -> String
-currentWord before after = reverse (takeWhile isAlpha (reverse before)) ++ takeWhile isAlpha after
-
 appEvent :: T.BrickEvent () e -> T.EventM () (EditorState ()) ()
 appEvent (T.VtyEvent (V.EvKey e m)) = keyEvent e m
 appEvent _ = return ()
 
+-- | Handles key events for the editor.
 keyEvent :: V.Key -> [V.Modifier] -> T.EventM () (EditorState ()) ()
 keyEvent (V.KChar 'c') [V.MCtrl] = M.halt
 keyEvent (V.KChar 's') [V.MCtrl] = dump
@@ -208,6 +197,7 @@ keyEvent V.KLeft _ = decrementIndex
 keyEvent V.KEsc _ = M.halt
 keyEvent _ _ = return ()
 
+-- | Deletes the character at the current cursor position in the editor.
 deleteChar :: T.EventM () (EditorState ()) ()
 deleteChar = do
   cursor <- use index
@@ -215,6 +205,7 @@ deleteChar = do
   decrementIndex
   changed .= True
 
+-- | Inserts the provided character at the current cursor position in the editor.
 insertChar :: Char -> T.EventM () (EditorState ()) ()
 insertChar c = do
   cursor <- use index
@@ -222,15 +213,18 @@ insertChar c = do
   index += 1
   changed .= True
 
+-- | Decrements the cursor index by one.
 decrementIndex :: T.EventM () (EditorState ()) ()
 decrementIndex = do
   index %= \i -> max 0 (i - 1)
 
+-- | Increments the cursor index by one.
 incrementIndex :: T.EventM () (EditorState ()) ()
 incrementIndex = do
   textLength <- use (text . to length)
   index %= \i -> min (i + 1) textLength
 
+-- | Saves the current state of the editor to a file.
 dump :: T.EventM () (EditorState ()) ()
 dump = do
   path' <- use path
@@ -238,9 +232,12 @@ dump = do
   liftIO $ writeFile path' text'
   changed .= False
 
-initialState :: FilePath -> Bool -> String -> EditorState ()
+-- | Creates the initial state of the editor.
+initialState :: String -> FilePath -> Bool -> EditorState ()
 initialState text' = EditorState text' (length text')
 
+-- | Defines the attribute map using the Brick library
+-- for defining the visual style of the user interface.
 theMap :: A.AttrMap
 theMap =
   A.attrMap
@@ -252,6 +249,8 @@ theMap =
       (A.attrName "error", V.white `on` V.red)
     ]
 
+-- | Defines the Brick application (named "editor"),
+-- using the application constructor from the Brick library.
 editor :: M.App (EditorState ()) e ()
 editor =
   M.App
@@ -262,6 +261,7 @@ editor =
       M.appAttrMap = const theMap
     }
 
+-- | Main entry point of a Haskell program.
 main :: IO ()
 main = do
   args <- getArgs
@@ -271,5 +271,5 @@ main = do
       let (text', changed') = case either' of
             Left _ -> ("", True)
             Right t -> (t, False)
-      void $ M.defaultMain editor (initialState path' changed' text')
+      void $ M.defaultMain editor (initialState text' path' changed')
     _ -> putStrLn "Usage: a3-exe <path>"
--- a3/src/Lib.hs
+++ a3/src/Lib.hs
@@ -1,4 +1,7 @@
-module Lib (insert', delete') where
+module Lib (insert', delete', listToSet, currentWord, splitAlphanumeric, countBraces) where
+
+import Data.Char (isAlpha, isAlphaNum)
+import Data.List (nub)
 
 init' :: [a] -> [a]
 init' [] = []
@@ -13,3 +16,25 @@ delete' :: Int -> [a] -> [a]
 delete' i cs =
   let (before, after) = splitAt i cs
    in init' before ++ after
+
+-- | Converts list of index tuples to a set without duplicates.
+listToSet :: [(Int, Int)] -> [Int]
+listToSet pairs = nub (concatMap (\(start, end) -> [start, end]) pairs)
+
+-- | 'currentWord' takes two strings, 'before' and 'after' (the cursor) and returns the word.
+currentWord :: String -> String -> String
+currentWord before after = reverse (takeWhile isAlpha (reverse before)) ++ takeWhile isAlpha after
+
+-- | 'splitAlphanumeric' takes a string and breaks it into a list of alphanumeric words.
+splitAlphanumeric :: String -> [String]
+splitAlphanumeric [] = []
+splitAlphanumeric (x : xs)
+  | isAlphaNum x =
+      let (alphanum, rest) = span isAlphaNum (x : xs)
+       in alphanum : splitAlphanumeric rest
+  | otherwise = [x] : splitAlphanumeric xs
+
+-- | Counts how often the given brace type occours in the String.
+-- Input: Input-String -> BraceType -> Count
+countBraces :: String -> Char -> Int
+countBraces xs x = foldl (\count char -> if char == x then count + 1 else count) 0 xs
